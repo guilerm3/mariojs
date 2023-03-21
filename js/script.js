d8fcffdc814c8ca9248b101e2b54b399b53f565a
Guilherme Lucas <gas7@outlook.com>
2023-03-21 03:15:36 -0300

    .

--- js/script.js
+++ js/script.js
@@ -12,12 +12,20 @@ function pular(){
 setInterval(function hitbox(){
   let tubo= document.getElementById('tubo')
   let mario= document.getElementById('mario')
+  let nuvem= document.getElementById('nuvem')
   let posicao_tubo= tubo.offsetLeft
   let posicao_mario= mario.offsetTop
+  let posicao_nuvens = nuvem.offsetLeft
   if(posicao_tubo <=145 && posicao_tubo >= -40 && posicao_mario >=315){
       tubo.style.animation = 'none'
-      tubo.style.left = '145px'
+      nuvem.style.left=`${posicao_nuvens}px`
+      tubo.style.left = `${posicao_tubo}px`
+      mario.src='img/esqueleto.png'
+      mario.style.top='485px'
+      clearInterval(hitbox)
     }
 },50)
+
+
       
  document.addEventListener('keydown', pular) 

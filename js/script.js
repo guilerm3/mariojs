afdc1d8862deadda5187128a627afcc69f656779
Guilherme Lucas <gas7@outlook.com>
2023-03-22 03:50:12 -0300

    .

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
 function pular(){
-    
-    mario.style.animation='animacaopulo 500ms linear'
-    
-        removeranimacao = function() {
-        mario.style.animation = ''
+  mario.style.animation='animacaopulo 500ms linear'
+    removeranimacao = function() {
+      mario.style.animation = ''
         mario.removeEventListener('animationend', removeranimacao)
       }
       mario.addEventListener('animationend', removeranimacao)
@@ -15,19 +13,26 @@ setInterval(function engine(){
   let posicao_nuvens = nuvem.offsetLeft
   if(posicao_tubo <=145 && posicao_tubo >= -40 && posicao_mario >=315){
       tubo.style.animation = 'none'
+      rolagem.style.animation = 'none'
       nuvem.style.left=`${posicao_nuvens}px`
       tubo.style.left = `${posicao_tubo}px`
       mario.src='img/esqueleto.png'
       mario.style.top='485px'
       botao.style.opacity='100%'
       clearInterval(engine)
-    }
-    if(posicao_tubo <-40 && posicao_tubo >-60){
-      placar.innerHTML= parseInt(placar.innerHTML)+1
-    }
+    } 
 },50)
 
+function placar(){
+  let contador=0
+  let tubo=document.getElementById('tubo')
+  let placar=document.getElementById('placar')
+  tubo.addEventListener('animationiteration', ()=>{
+    contador++
+    placar.innerHTML=contador
+  })
+}
 
-      
+ window.addEventListener('load', placar);   
  document.addEventListener('keydown', pular) 
  document.addEventListener('touchstart', pular) 
